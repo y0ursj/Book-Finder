6ee5319229629605bb22cef2a98b8f3b498ed082
Sujith  John <sujithjohn@Sujiths-MacBook-Air.local>
2019-10-03 11:26:31 -0400

    saveeeeed

--- app.js
+++ app.js
@@ -13,14 +13,14 @@ let renderList = (books) => {
     let el = document.createElement('div')
     el.classList.add('book1');
     el.innerHTML = `<img src=${books[i].volumeInfo.imageLinks.thumbnail}><div class="book-info"><h2>Title: ${books[i].volumeInfo.title}</h2><h3>Author: ${books[i].volumeInfo.authors[0]}</
-   </h3><h4>Reader Ratings: ${books[i].volumeInfo.averageRating}</h4><h5 id="textInfo">${books[i].searchInfo.textSnippet}</h5></div>`
+   </h3><h4>Reader Ratings: ${books[i].volumeInfo.averageRating}</h4><h4 id="textInfo">${books[i].searchInfo.textSnippet}</h4><h4>Page Count: ${books[i].volumeInfo.pageCount}</h4></div><div id="buy-button">${(books[i].saleInfo.buyLink ? `<a href=${books[i].saleInfo.buyLink}><button>Buy</button></a>` : `<p>Not for sale</p>`)}<div>`
     list.append(el)
   }
 }
 
 button.addEventListener("click", async function () {
   const response = await axios.get(`${BASE_URL}${input.value}${API_Key}`)
-console.log(response.data.items)
+  console.log(response.data.items)
   renderList(response.data.items);
 })
 
